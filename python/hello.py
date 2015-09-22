dc36cddc866811786bfe663ff4b8a47156966568
Dushyant Kashyap <dushyantk@gmail.com>
2015-09-23 00:05:05 +0530

    completed hello dJango module

--- python/hello.py
+++ python/hello.py
@@ -1,12 +1,39 @@
-set the environment
+#!/usr/bin/env python
 
-import stuff
+import sys
+from django.conf import settings
+
+# Settings' section contains everything from database and cache connections to
+# static and uploaded resources. But here we have Debug mode ON and then
+# urlpattern will do it's magic.
+# We have here a nonrandom SECRET_KEY setting
+# Which should not be used in a production environment
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='thisisthesecretkey',
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
 
 
-configure settings
+from django.conf.urls import url
+from django.http import HttpResponse
 
-response procedure
+#Typical content of views.py in a larger project
+dief index(request):
+    return HttpResponse('Hello dJango')
 
-define url pattern
+#Typical content of urls.py
+urlpatterns = (
+    url(r'^$', index),
+)
 
-execute main as server
+#manage.py works somewhat like this
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
