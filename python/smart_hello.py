7fb1f92324a0b69bae142a6b192ae81b3c88f1ff
Dushyant Kashyap <dushyantk@gmail.com>
2015-09-23 20:30:45 +0530

    Adding refined version of hello.py

---
+++ python/smart_hello.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+from django.conf import settings
+
+# DEBUG value is set in os environment
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+# Secret key is random and thus secure
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+
+# Take care of this during deployment
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+settings.configure(
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello World')
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+application = get_wsgi_application()
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+execute_from_command_line(sys.argv)
